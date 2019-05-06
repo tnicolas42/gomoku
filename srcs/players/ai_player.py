4bc17cbea822847215326ded32eb1b358752730a
zer0nim <emarin@e2r2p23.42.fr>
2019-05-06 11:07:54 +0200

    Put the stone of the computer

--- srcs/players/ai_player.py
+++ srcs/players/ai_player.py
@@ -12,19 +12,10 @@ class AIPlayer(Player):
         this function is called when the AI need to move
         -> put a stone on the board
         """
-        # first algo -> just put a random stone
-        # time.sleep(0.1)
-        # while 42:
-        #     x = random.randrange(0, self.game.board.size)
-        #     y = random.randrange(0, self.game.board.size)
-        #     if self.game.board.is_allowed(x, y, self.stone):
-        #         self.game.board.put_stone(x, y, self.stone)
-        #         return
-        # print(self.game.boaboard)
-
-        depth = 3
+        depth = 2
         nodes = Node(self.game, self.stone, -1, -1, depth, None)
-        print(min_max(nodes, depth, True))
+        move = min_max(nodes, depth, True)
+        self.game.board.put_stone(move['node'].x, move['node'].y, self.stone)
 
 def heuristic(node):
     return random.randint(-1000, 1000)
@@ -37,7 +28,7 @@ def min_max(node, depth, maximize):
     min_max algorithm implementation
     """
     if depth == 0 or is_terminal_node(node):
-        return {'_node': node, 'cost': heuristic(node)}
+        return {'node': node, 'cost': heuristic(node)}
     if maximize:
         _max = float('-inf')
         for child in node.childs:
@@ -45,7 +36,7 @@ def min_max(node, depth, maximize):
             if childMin['cost'] > _max:
                 _max = childMin['cost']
                 _node = child
-        return {'_node': _node, 'cost': _max}
+        return {'node': _node, 'cost': _max}
     else:
         _min = float('inf')
         for child in node.childs:
@@ -53,4 +44,4 @@ def min_max(node, depth, maximize):
             if childMax['cost'] < _min:
                 _min = childMax['cost']
                 _node = child
-        return {'_node': _node, 'cost': _min}
+        return {'node': _node, 'cost': _min}
