e0f3f25b3797fd8c03af9a8f4b0be5ff61ef10e5
zer0nim <emarin@e3r12p22.42.fr>
2019-06-04 11:16:25 +0200

    Small optimization *2 => <<1

--- srcs/utils/u_board.py
+++ srcs/utils/u_board.py
@@ -4,13 +4,13 @@ def _at(board, x, y):
     '''
         Return the content at the board given position
     '''
-    return (board[y] & (0b11 << x*2)) >> x*2
+    return (board[y] & (0b11 << (x<<1))) >> (x<<1)
 
 def _set(board, x, y, val):
     '''
         Set the content of the board given position
     '''
     # clear the position
-    board[y] = board[y] & (((1 << G.GET("BOARD_SZ")*2)-1) ^ (0b11 << x*2))
+    board[y] = board[y] & (((1 << (G.GET("BOARD_SZ")<<1))-1) ^ (0b11 << (x<<1)))
     # fill the position
-    board[y] = board[y] | (val << x*2)
+    board[y] = board[y] | (val << (x<<1))
